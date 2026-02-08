d4593d120d792cadf54aeca87968c1171a707c9f
Ishaan Singh <theishaansingh7@gmail.com>
2026-02-08 23:24:31 +0530

    Day8 Q1

---
+++ Day8Q1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+long long power(int a, int b) {
+    if (b == 0) {
+        return 1;
+    }
+    
+    long long half = power(a, b / 2);
+    
+    if (b % 2 == 0) {
+        return half * half;
+    } else {
+        return a * half * half;
+    }
+}
+
+int main() {
+    int a, b;
+    if (scanf("%d %d", &a, &b) == 2) {
+        printf("%lld\n", power(a, b));
+    }
+    return 0;
+}
