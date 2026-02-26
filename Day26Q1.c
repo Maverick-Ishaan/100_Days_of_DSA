f4a90035da363fd87105a83f51fd5b17a26b78d3
Ishaan Singh <theishaansingh7@gmail.com>
2026-02-26 22:53:04 +0530

    Day26Q1

---
+++ Day26Q1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+
+int main() {
+    int n, value;
+    struct Node *head = NULL, *tail = NULL;
+
+    if (scanf("%d", &n) != 1 || n <= 0) return 0;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+        newNode->prev = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+    }
+
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d%s", temp->data, (temp->next ? " " : ""));
+        temp = temp->next;
+    }
+    printf("\n");
+
+    temp = head;
+    while (temp != NULL) {
+        struct Node* nextNode = temp->next;
+        free(temp);
+        temp = nextNode;
+    }
+
+    return 0;
+}
