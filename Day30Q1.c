4e60d32a220f31faf3270eae29eb1ecd5d87bb3c
Ishaan Singh <theishaansingh7@gmail.com>
2026-03-02 11:11:25 +0530

    Day30Q1

---
+++ Day30Q1.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int coeff;
+    int exp;
+    struct Node* next;
+};
+
+struct Node* createNode(int c, int e) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->coeff = c;
+    newNode->exp = e;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void display(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        if (temp->exp == 0) {
+            printf("%d", temp->coeff);
+        } else if (temp->exp == 1) {
+            printf("%dx", temp->coeff);
+        } else {
+            printf("%dx^%d", temp->coeff, temp->exp);
+        }
+
+        temp = temp->next;
+        if (temp != NULL) {
+            printf(" + ");
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    int n, c, e;
+    struct Node *head = NULL, *tail = NULL;
+
+    if (scanf("%d", &n) != 1) return 0;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &c, &e);
+        struct Node* newNode = createNode(c, e);
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    display(head);
+
+    struct Node* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+
+    return 0;
+}
