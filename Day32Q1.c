6955bc3486e485e5020588fed0bb4c7c75d1f7d9
Ishaan Singh <theishaansingh7@gmail.com>
2026-03-04 17:42:25 +0530

    Day32Q1

---
+++ Day32Q1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n, m;
+
+    if (scanf("%d", &n) != 1) return 0;
+
+    int stack[n];
+    int top = -1;
+
+    for (int i = 0; i < n; i++) {
+        int val;
+        scanf("%d", &val);
+        stack[++top] = val;
+    }
+
+    if (scanf("%d", &m) != 1) return 0;
+
+    for (int i = 0; i < m; i++) {
+        if (top >= 0) {
+            top--;
+        }
+    }
+
+    for (int i = top; i >= 0; i--) {
+        printf("%d", stack[i]);
+        if (i > 0) printf(" ");
+    }
+    printf("\n");
+
+    return 0;
+}
