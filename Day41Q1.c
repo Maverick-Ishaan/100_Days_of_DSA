1b4f56142b29be575746e704d3b7a4f224f00145
Ishaan Singh <theishaansingh7@gmail.com>
2026-03-13 22:25:45 +0530

    Day41Q1

---
+++ Day41Q1.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Queue {
+    struct Node *front, *rear;
+};
+
+struct Queue* createQueue() {
+    struct Queue* q = (struct Queue*)malloc(sizeof(struct Queue));
+    q->front = q->rear = NULL;
+    return q;
+}
+
+void enqueue(struct Queue* q, int value) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = value;
+    temp->next = NULL;
+    if (q->rear == NULL) {
+        q->front = q->rear = temp;
+        return;
+    }
+    q->rear->next = temp;
+    q->rear = temp;
+}
+
+int dequeue(struct Queue* q) {
+    if (q->front == NULL) {
+        return -1;
+    }
+    struct Node* temp = q->front;
+    int data = temp->data;
+    q->front = q->front->next;
+    if (q->front == NULL) {
+        q->rear = NULL;
+    }
+    free(temp);
+    return data;
+}
+
+int main() {
+    int n;
+    if (scanf("%d", &n) != 1) return 0;
+    struct Queue* q = createQueue();
+
+    for (int i = 0; i < n; i++) {
+        char op[10];
+        scanf("%s", op);
+        if (strcmp(op, "enqueue") == 0) {
+            int val;
+            scanf("%d", &val);
+            enqueue(q, val);
+        } else if (strcmp(op, "dequeue") == 0) {
+            printf("%d\n", dequeue(q));
+        }
+    }
+
+    return 0;
+}
